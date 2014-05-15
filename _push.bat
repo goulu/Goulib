219ae539043f29fbbd5fff44a5e94d11a2d76fce
unknown <GUGLIE0P@BSALCZC14262ZS.grpdom.dsbgrp.com>
2014-05-15 09:45:58 +0200

    1.7.2

--- _push.bat
+++ _push.bat
@@ -1,8 +1,5 @@
-global /i del *.pyc
-
 git push https://github.com/goulu/Goulib.git
 git push https://goulu@bitbucket.org/goulu/goulib.git
 git push ssh://drgoulu@git.code.sf.net/p/goulib/code
-git commit googlecode master
-
+git push https://drgoulu@code.google.com/p/goulib/
 svn commit --username=drgoulu svn+ssh://drgoulu@svn.code.sf.net/p/goulib/code-0/trunk
