0fd198289add665be59c411ee419c775a1fc205c
unknown <guglie0p@BSALCZC14262ZS.grpdom.dsbgrp.com>
2014-06-05 10:49:23 +0200

    tests added

--- tests/test_Goulib_markup.py
+++ tests/test_Goulib_markup.py
@@ -191,5 +191,15 @@ class test__oneliner:
         # _oneliner = _oneliner(case)
         raise SkipTest
 
+class test__oneliner:
+    def test___getattr__(self):
+        # _oneliner = _oneliner(case)
+        # assert_equal(expected, _oneliner.__getattr__(attr))
+        raise SkipTest
+
+    def test___init__(self):
+        # _oneliner = _oneliner(case)
+        raise SkipTest
+
 if __name__=="__main__":
     runmodule()
