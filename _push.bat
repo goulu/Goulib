ed6371c44dde2a0b694da1155dc402c2d4af6fdf
Philippe Guglielmetti <github@goulu.net>
2014-11-07 09:52:27 +0100

    Delete _push.bat

--- _push.bat
+++
@@ -1,5 +0,0 @@
-git push https://github.com/goulu/Goulib.git
-git push https://goulu@bitbucket.org/goulu/goulib.git
-git push ssh://drgoulu@git.code.sf.net/p/goulib/code
-git push https://drgoulu@code.google.com/p/goulib/
-svn commit --username=drgoulu svn+ssh://drgoulu@svn.code.sf.net/p/goulib/code-0/trunk
