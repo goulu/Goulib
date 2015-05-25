b2e8415f60a4d833adbedf334961092fb66afcc9
Philippe Guglielmetti <github@goulu.net>
2015-05-25 16:44:00 +0200

    is_prime now implements Miller-Rabin test for large numbers

--- Goulib/math2.py
+++ Goulib/math2.py
@@ -247,15 +247,49 @@ def proper_divisors(n):
     """:return: all divisors of n except n itself."""
     return (divisor for divisor in divisors(n) if divisor != n)
 
-def is_prime(n):
+
+ 
+def is_prime(n, _precision_for_huge_n=16):
     """:return: True if n is a prime number (1 is not considered prime)."""
-    if n < 3:
-        return (n == 2)
-    elif n % 2 == 0:
+    # http://rosettacode.org/wiki/Miller-Rabin_primality_test#Python
+    if n in (0, 1):
         return False
-    elif any(((n % x) == 0) for x in range(3, int(sqrt(n))+1, 2)):
+    if n in _known_primes:
+        return True
+    if any((n % p) == 0 for p in _known_primes):
         return False
-    return True
+    d, s = n - 1, 0
+    while not d % 2:
+        d, s = d >> 1, s + 1
+    # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+    def _try_composite(a, d, n, s):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n-1:
+                return False
+        return True # n  is definitely composite
+    
+    if n < 1373653: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3))
+    if n < 25326001: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
+    if n < 118670087467: 
+        if n == 3215031751: 
+            return False
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+    if n < 2152302898747: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+    if n < 3474749660383: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+    if n < 341550071728321: 
+        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+    # otherwise
+    return not any(_try_composite(a, d, n, s) 
+        for a in _known_primes[:_precision_for_huge_n])
+ 
+_known_primes = [2, 3]
+_known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
 
 def get_primes(start=2, memoized=False):
     from .decorators import memoize
