3e5d6bc13d7f2541bc4e8a9d19b8bcaa99ea9162
Philippe Guglielmetti <guglie0p@bobst.com>
2015-10-15 14:40:05 +0200

    circumvented potential unicode problem

--- Goulib/tests.py
+++ Goulib/tests.py
@@ -44,21 +44,16 @@ def pprint_gen(iterable,indices=[0,1,2,-3,-2,-1],sep='...'):
                 if sep : 
                     yield sep 
 
-def pprint(iterable,indices=[0,1,2,3,4,5,6,7,8,9,-3,-2,-1],timeout=1000):
+def pprint(iterable,indices=[0,1,2,3,4,5,6,7,8,9,-3,-2,-1],timeout=1):
     sep='...'
     s=[]
     try:
         items=pprint_gen(iterable, indices, sep)
         for item in decorators.itimeout(items, timeout): 
-            s.append(str(item))
+            s.append('%s'%item)
     except decorators.TimeoutError:
         if s[-1]!=sep:
             s.append(sep)
-    """        
-    except Exception as e:
-        s.append(str(e))
-        raise e
-    """
     return ','.join(s)
     
 
