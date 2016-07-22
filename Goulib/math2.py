c3298d5775a4a1af96c4891be33dd6422f45be98
Philippe Guglielmetti <guglie0p@bobst.com>
2016-07-22 13:42:48 +0200

    added de_bruijn sequence

--- Goulib/math2.py
+++ Goulib/math2.py
@@ -1269,6 +1269,37 @@ def rectangular_repartition(x,n,h):
 
     return [_integral(i*w,(i+1)*w) for i in range(n)]
 
+def de_bruijn(k, n):
+    """
+    De Bruijn sequence for alphabet k and subsequences of length n.
+    https://en.wikipedia.org/wiki/De_Bruijn_sequence
+    """
+    try:
+        # let's see if k can be cast to an integer;
+        # if so, make our alphabet a list
+        _ = int(k)
+        alphabet = list(map(str, range(k)))
+
+    except (ValueError, TypeError):
+        alphabet = k
+        k = len(k)
+
+    a = [0] * k * n
+    sequence = []
+
+    def db(t, p):
+        if t > n:
+            if n % p == 0:
+                sequence.extend(a[1:p + 1])
+        else:
+            a[t] = a[t - p]
+            db(t + 1, p)
+            for j in range(a[t - p] + 1, k):
+                a[t] = j
+                db(t + 1, t)
+    db(1, 1)
+    return "".join(alphabet[i] for i in sequence)
+
 """modular arithmetic
 initial motivation: https://www.hackerrank.com/challenges/ncr
 
