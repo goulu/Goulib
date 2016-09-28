7111482e6198d61b19bac427788478bdeac43015
Philippe Guglielmetti <guglie0p@bobst.com>
2016-09-28 14:34:19 +0200

    latex function added to force latex output

--- Goulib/notebook.py
+++ Goulib/notebook.py
@@ -9,6 +9,8 @@ exemple: h('the result is',time,'[ms]')
 
 """
 
+from __future__ import print_function
+
 __author__ = "Philippe Guglielmetti"
 __copyright__ = "Copyright 2016, Philippe Guglielmetti"
 __credits__= [""]
@@ -16,7 +18,7 @@ __license__ = "LGPL"
 
 import logging, six
 
-from IPython.display import display, HTML
+from IPython.display import display, HTML, Math
 from .markup import tag
 from .itertools2 import isiterable
 
@@ -26,7 +28,7 @@ def html(obj, sep=None):
         return obj._repr_html_()
     except AttributeError:
         pass #skip logging.error
-    
+
     if sep is None:
         sep=' '
         bra,ket='',''
@@ -37,45 +39,50 @@ def html(obj, sep=None):
             bra,ket='{','}'
         else:
             bra,ket='(',')'
-    
+
     if isiterable(obj): #iterable, but not a string
             return bra+sep.join(html(a,sep=',') for a in obj)+ket
-    
+
     try:
         return unicode(obj,'utf-8') #to render accented chars correctly
     except :
         pass
-    
+
     return str(obj)
 
 def h1(*args):
     display(HTML(tag('h1',html(args))))
-    
+
 def h2(*args):
     display(HTML(tag('h2',html(args))))
-    
+
 def h3(*args):
     display(HTML(tag('h3',html(args))))
-    
+
 def h4(*args):
     display(HTML(tag('h4',html(args))))
-    
+
 def h(*args):
     display(HTML(html(args)))
-    
+
 #redefine "print" for notebooks ...
 try: #http://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
     get_ipython #is defined from within IPython (notebook)
 except:
     pass
 else:
-    print = h
-    
-def hinfo(*args):   
+    pass #for pythoscope
+    # print = h # this is ok in Python 3, but not before
+
+def hinfo(*args):
     display(HTML(tag('div',html(args),style="background-color:#337ab7;color:#ffffff")))
-def hsuccess(*args):   
+def hsuccess(*args):
     display(HTML(tag('div',html(args),style="background-color:#5cb85c;color:#ffffff")))
-def hwarning(*args):   
+def hwarning(*args):
     display(HTML(tag('div',html(args),style="background-color:#f0ad4e;color:#ffffff")))
-def herror(*args):   
+def herror(*args):
     display(HTML(tag('div',html(args),style="background-color:#d9534f;color:#ffffff")))
+
+def latex(obj):
+    """ to force LaTeX representation """
+    return Math(obj.latex())
