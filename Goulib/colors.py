7d685535de6c32ed6104161947d1b4e5c61e8ff6
Guglielmetti Philippe <github@goulu.net>
2018-10-26 15:57:17 +0200

    added lambda2RGB and RGB2lambda

--- Goulib/colors.py
+++ Goulib/colors.py
@@ -569,3 +569,93 @@ def color_range(n,start,end,space='hsv'):
     start=Color(start).convert(space)
     end=Color(end).convert(space)
     return [Color(v, space=space) for v in linspace(start,end,n)]
+
+def lambda2RGB(wavelength):
+    # http://codingmess.blogspot.com/2009/05/conversion-of-wavelength-in-nanometers.html 
+    w = int(wavelength)
+
+    # colour
+    if w >= 380 and w < 440:
+        R = -(w - 440.) / (440. - 350.)
+        G = 0.0
+        B = 1.0
+    elif w >= 440 and w < 490:
+        R = 0.0
+        G = (w - 440.) / (490. - 440.)
+        B = 1.0
+    elif w >= 490 and w < 510:
+        R = 0.0
+        G = 1.0
+        B = -(w - 510.) / (510. - 490.)
+    elif w >= 510 and w < 580:
+        R = (w - 510.) / (580. - 510.)
+        G = 1.0
+        B = 0.0
+    elif w >= 580 and w < 645:
+        R = 1.0
+        G = -(w - 645.) / (645. - 580.)
+        B = 0.0
+    elif w >= 645 and w <= 780:
+        R = 1.0
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+
+    # intensity correction
+    if w >= 380 and w < 420:
+        SSS = 0.3 + 0.7*(w - 350) / (420 - 350)
+    elif w >= 420 and w <= 700:
+        SSS = 1.0
+    elif w > 700 and w <= 780:
+        SSS = 0.3 + 0.7*(780 - w) / (780 - 700)
+    else:
+        SSS = 0.0
+    SSS *= 255
+
+    return [int(SSS*R), int(SSS*G), int(SSS*B)]
+
+def RGB2lambda(R, G, B):
+    # http://codingmess.blogspot.com/2010/02/transforming-rgb-data-to-wavelength.html
+    """Returns 0 if indeciferable"""
+    # selects range by maximum component
+    # if max is blue - range is 380 - 489
+    # if max is green - range is 490 - 579
+    # if max is red - range is 580 - 645
+    
+    # which colour has highest intensity?
+    high = float(R)
+    highind = 1
+    if G > high:
+        high = float(G)
+        highind = 2
+    if B > high:
+        high = float(B)
+        highind = 3
+       
+    # normalize highest to 1.0
+    RGBnorm = [R / high, G / high, B / high]
+    
+    # start decoding
+    RGBlambda = 0
+    if highind == 1: # red is highest
+        if B >= G: # there is more blue than green
+            return 0 # max red and more blue than green shouldn't happen
+        # wavelength linearly changes from 645 to 580 as green increases
+        RGBlambda = 645 - RGBnorm[1] * (645. - 580.)
+       
+    elif highind == 2: # green is max, range is 510 - 579
+        if R > B: # range is 510 - 579
+            RGBlambda = 510 + RGBnorm[0] * (580 - 510)
+        else: # blue is higher than red, range is 490 - 510
+            RGBlambda = 510 - RGBnorm[2] * (510 - 490)
+           
+    elif highind == 3: # blue is max, range is 380 - 490
+        if G > R: # range is 440 - 490
+            RGBlambda = RGBnorm[1] * (490 - 440) + 440
+        else: # there is more red than green, range is 380 - 440
+            RGBlambda = 440 - RGBnorm[0] * (440 - 380)
+    
+    return RGBlambda
