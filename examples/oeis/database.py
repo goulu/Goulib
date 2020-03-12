7cf9d6484ac35989ade5de4c2f86c236f7ebff14
Guglielmetti Philippe <github@goulu.net>
2020-03-12 09:54:26 +0100

    Database.populate automatically from http://oeis.org/stripped.gz

--- examples/oeis/database.py
+++ examples/oeis/database.py
@@ -26,20 +26,33 @@ class Database:
                 logging.info("tables created")
         except sqlite3.Error as error:
             logging.error("Error while connecting to sqlite", error)
-            
+
     def execute(self, sql):
         cursor = self.db.cursor()
         cursor.execute(sql)
-        res = cursor.fetchall() 
+        res = cursor.fetchall()
         cursor.close();
         return res
-    
-    @property        
+
+    @property
     def version(self):
         return str(self.execute("select sqlite_version();")[0][0])
-    
+
     def __setitem__(self, key, value):
         key = int(key[1:])
+        value=list(value)
+        
+        #check if already existing (for performance)
+        s = "SELECT MAX(i),repeat FROM seq WHERE id=%d" % key
+        imax = self.execute(s)
+        try:
+            imax=sum(imax[0])-1
+            if imax>=len(value): #already stored
+                return
+        except:
+            pass # sequence not in db
+        
+        #store it
         cursor = self.db.cursor()
         req = "INSERT OR REPLACE INTO seq (id, i, n, repeat) VALUES"
         i = 1
@@ -49,37 +62,57 @@ class Database:
             i = i + r
         cursor.close();
         self.db.commit();
-    
+
     def __getitem__(self, key):
         key = int(key[1:])
         s = "SELECT n,repeat FROM seq WHERE id=%d ORDER BY i" % key
         res = self.execute(s)
-        if not res : 
+        if not res :
             raise IndexError
         return itertools2.decompress(res)
-    
+
     def search(self, value):
-        #efficient search first requires to dynamically build a tricky request 
+        # efficient search first requires to dynamically build a tricky request
         for j, (n, r) in enumerate(itertools2.compress(value)):
             if j == 0:
                 s1 = "WITH q0 AS (SELECT * FROM seq WHERE n = %d AND repeat >= %d)," % (n, r)
                 s2 = "SELECT q0.id, q0.i FROM q0"
-            else: 
+            else:
                 s1 = s1 + "\nq%d AS (SELECT * FROM seq WHERE n = %d AND repeat = %d)," % (j, n, r)
                 s2 = s2 + '\nJOIN q%d ON q%d.id=q0.id AND q%d.i=q%d.i+q%d.repeat' % tuple([j] * 3 + [j - 1] * 2)
         s = s1[:-1] + '\n\n' + s2
-        res=self.execute(s)
+        res = self.execute(s)
         # keep first result for each sequence only
-        for (id,i) in itertools2.unique(res, key=lambda x:x[0], buffer=None):
-            key='000000'+str(id)
-            yield 'A'+key[-6:],i
+        for (id, i) in itertools2.unique(res, key=lambda x:x[0], buffer=None):
+            key = '000000' + str(id)
+            yield 'A' + key[-6:], i
 
-        
+    def populate(self, file='http://oeis.org/stripped.gz'):
+        import gzip
+        if file[:5] == 'http:':
+            from urllib.request import urlopen
+            file = urlopen(file)
+
+        file = gzip.open(file)
+
+        for line in file:
+            l=line.decode("utf-8") 
+            if l[0]!='A':
+                logging.info(line)
+                continue
+            l=l.split(',')
+            id=l[0]
+            value=map(int,l[1:-1]) # skip the last \n
+            logging.debug(id)
+            self[id]=value
+
+logging.basicConfig(level=logging.DEBUG)
 path = os.path.dirname(os.path.abspath(__file__))
 database = Database(path + '/oeis.db')
 logging.info("SQLite DB Version : " + database.version)
+database.populate() # 'stripped.gz'
 
-from Goulib.tests import *
+# from Goulib.tests import *
 
 
 class TestDatabase:
