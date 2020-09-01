9107f27bd742831d3998fde2db765bed91d38710
Guglielmetti Philippe <github@goulu.net>
2020-09-01 11:49:40 +0200

    Python 3.5 no more supported because of a dependency

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
